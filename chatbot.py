8fec8fd8c185f347020a96a751e6f798a8020eed
Keshav <keshavjha018@gmail.com>
2021-11-17 18:47:16 +0530

    edited chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -4,15 +4,20 @@ info = ['Sir, i am your desktop assistant, My name is Walter. What can i do for 
 
 command_greet = ['hello Walter','are you there Walter','are you there','hello']
 greet = ['hello sir. how may i help you','i am here for your help','yes sir, how may i help you']
+
 command_quit = ['close the program', 'quit the program', 'terminate the program', 'exit the program'
                 'close program', 'quit program', 'terminate program', 'exit program','close','quit','exit']
 command_quit_replay = ['closing the program', 'quiting the program',
                        'terminating the program', 'exiting the program']
+
 chat = ['good morning Walter','morning Walter']
 chat_replay = ['good morning sir i hope your day will be good','good morning sir how amy i help you']
+
 chat_2 = ['thankyou Walter','thankyou']
 chat_2_replay =['your welcome','my pleasure','enjoy your day, sir']
+
 chat_3 = ['you are idiot','what had you done']
 chat_3_replay = ['sorry sir,i apologize','sorry sir']
+
 chat_4 = ['nice work','nice work Walter','good work','its awesome']
 chat_4_replay = ['Thank you, sir', 'nice to hear from you, sir', 'my pleasure']
