9d239f32e9d71e119ea1a385b4fc69fdbcafb344
Hemant <20bcs058@iiitdwd.ac.in>
2021-11-26 10:51:48 +0530

    added battery status

--- features/basic.py
+++ features/basic.py
@@ -1,6 +1,6 @@
 import pyautogui
 from time import sleep
-
+import wmi
 state = ["Speaking..."]
 chat = []
 chat_prev = []
@@ -31,3 +31,20 @@ def chatUser(query):
     chat_prev = chat.copy()
     chat.append("User: " + query + "\n")
     sleep(1)
+
+def battery():
+    c = wmi.WMI()
+    t = wmi.WMI(moniker = "//./root/wmi")
+
+    batts1 = c.CIM_Battery(Caption = 'Portable Battery')
+    batts = t.ExecQuery('Select * from BatteryFullChargedCapacity')
+    for i, b in enumerate(batts):
+        full=(b.FullChargedCapacity)
+
+    batts = t.ExecQuery('Select * from BatteryStatus where Voltage > 0')
+    for i, b in enumerate(batts): 
+      rem=(b.RemainingCapacity)
+    
+    per=int((rem/full)*100)
+    return per
+   
